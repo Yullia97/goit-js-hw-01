3c05ac979a135cdc1e05622b7617b03ca261bc5b
Yullia97 <146541652+Yullia97@users.noreply.github.com>
2025-01-05 17:48:56 +0200

    copy

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,16 @@
 function getElementWidth(content, padding, border)
 {
-  // Перетворення значень із формату "Npx" на числа
+ 
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
 
-  // Розрахунок загальної ширини елемента
+
    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
     return totalWidth;
 }
 
-// Перевірка роботи функції
+
 
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
